986385e4b79c468af57b0f4c3dca80dea2d69363
xiezihen <frank.xie@mail.utoronto.ca>
2023-04-05 02:36:30 -0400

    Update my-module.js

--- public/my-module.js
+++ public/my-module.js
@@ -1,4 +1,4 @@
-import { theShire, buckland } from './another-module';
+import { theShire, buckland } from './another-module.js';
 
 const statement = `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
 
