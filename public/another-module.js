1a5cb0e4f9380e57366ec4c7add7e30a08312934
xiezihen <frank.xie@mail.utoronto.ca>
2023-04-05 01:59:42 -0400

    Update another-module.js

--- public/another-module.js
+++ public/another-module.js
@@ -1,12 +1,12 @@
-var HobbitSettlement = class {
+const HobbitSettlement = class {
   constructor(name, founding, founders) {
     this.name = name;
     this.founding = founding;
     this.founders = founders;
-    this.originalSettlement = { name: "Eriador", details: eriadorDetails };
+    this.originalSettlement = { name: 'Eriador', details: eriadorDetails };
   }
 };
 
-var theShire = new HobbitSettlement("The Shire", 1601, ["Marcho", "Blanco"]);
-var buckland = new HobbitSettlement("Buckland", 2340, "Gorhendad Oldbuck");
+const theShire = new HobbitSettlement("The Shire", 1601, ["Marcho", "Blanco"]);
+const buckland = new HobbitSettlement("Buckland", 2340, "Gorhendad Oldbuck");
 export { HobbitSettlement, theShire, buckland };
